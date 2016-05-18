39a440a954fc877ea791f3f40eb2d716687273fd
simonppg <simonppg@gmail.com>
2016-05-18 14:54:12 -0500

    Usando API restful desde el cliente

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,27 @@
 import express from 'express'
 const app = express()
 
+const votes = {
+	"id show": 0
+}
+
 //express sirve los archivos estaticos
 app.use(express.static('public'))
 
 // GET /votes
 app.get('/votes', (req, res) => {
-	res.json([])
+	res.json(votes)
 })
 
 // POST /vote/<id>
 app.post('/vote/:id', (req, res) => {
-	
+	let id = req.params.id
+	if (votes[id] === undefined)
+		votes[id] = 1
+	else
+		votes[id] = votes[id] + 1
+
+	res.json({ votes: votes[id] })
 })
 
 app.listen(3000, () => console.log('Servidor express iniciado. Escuchando en el puerto 3000'))
