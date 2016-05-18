3fe82f6ecce9bdc4a23d124de1df7fc64a165eb3
simonppg <simonppg@gmail.com>
2016-05-18 14:03:23 -0500

    Agregando es2015

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,17 @@
-var express	= require('express')
-var app		= express()
+import express from 'express'
+const app = express()
 
 //express sirve los archivos estaticos
 app.use(express.static('public'))
 
 // GET /votes
-app.get('/votes', function (req, res) {
+app.get('/votes', (req, res) => {
 	res.json([])
 })
 
 // POST /vote/<id>
-app.post('/vote/:id', function (req, res) {
+app.post('/vote/:id', (req, res) => {
 	
 })
 
-app.listen(3000, function(){
-	console.log('Servidor express iniciado. Escuchando en el puerto 3000')
-})
+app.listen(3000, () => console.log('Servidor express iniciado. Escuchando en el puerto 3000'))
